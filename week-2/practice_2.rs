3ad10ca6f5bccd8c10e137db17fce428cc1dc584
str4dave <davekome07@gmail.com>
2024-10-27 13:04:43 +0100

    (my second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main () {
+
+	println!(); //prints just a
+
+	println!("hello ");//prints
+
+	println!("format {} narguments", "some"); //prints format some agrguments
+
+	println!("My name is {}. I am the class rep of stream 1", "David Okiomah")
+
+}
